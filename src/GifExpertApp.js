2eebe5d6c9bd55829c95af84dd7d0e3080596cc9
Christian Dionisio <christiandionisio9432@gmail.com>
2022-03-20 17:00:37 -0500

    Comunicacion entre componentes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -6,14 +6,10 @@ const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
-    // const handleAdd = () => {
-    //     setCategories(['HunterXHunter', ...categories] );
-    // }
-
     return (
         <>
             <h2>GifExpertApp</h2>
-            <AddCategory />
+            <AddCategory setCategories={setCategories} />
             <hr />
 
             <ol>
