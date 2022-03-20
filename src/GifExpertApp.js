d2bdd6dc208f0bc05ff9a42054d33d6d8aeba829
Christian Dionisio <christiandionisio9432@gmail.com>
2022-03-20 16:49:01 -0500

    Componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 
 const GifExpertApp = () => {
 
-    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
-    const handleAdd = () => {
-        setCategories(['HunterXHunter', ...categories] );
-    }
+    // const handleAdd = () => {
+    //     setCategories(['HunterXHunter', ...categories] );
+    // }
 
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory />
             <hr />
-
-            <button onClick={handleAdd}>Agregar</button>
 
             <ol>
                 {
