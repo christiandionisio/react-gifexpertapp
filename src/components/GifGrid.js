3e89bc78a97387bcb8d4089249c801dbd55f55af
Christian Dionisio <christiandionisio9432@gmail.com>
2022-03-20 18:09:53 -0500

    obtener imagenes y estado de carga

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,42 +1,29 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs';
-// import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-    // const [images, setImages] = useState([]);
+    const {data:images, loading} = useFetchGifs(category);  
 
-    const {data, loading} = useFetchGifs();
+    return (
+        <>
+            <h3>{category}</h3>
 
-    console.log(loading);
-    console.log(data);
+            {loading && <p>Loading</p>}
 
-
-    // useEffect(() => {
-
-    //     getGifs(category).then(setImages);
-
-    // }, [category]);
-
-    
-
-  return (
-    <>
-        <h3>{category}</h3>
-        {loading ? 'Cargando...' : 'Data cargada'}
-        {/* <div className='card-grid'>
-            
-            {
-                images.map((img) => 
-                    <GifGridItem 
-                        key={img.id} 
-                        {...img} 
-                    />
-                )
-            }
-            
-        </div> */}
-    </>
+            <div className='card-grid'>
+                
+                {
+                    images.map((img) => 
+                        <GifGridItem 
+                            key={img.id} 
+                            {...img} 
+                        />
+                    )
+                }
+                
+            </div>
+        </>
   )
 }
